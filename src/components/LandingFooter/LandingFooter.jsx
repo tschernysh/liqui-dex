3446163d52eefce5c2adff858adea752c22d4ffd
tschernysh <tschernysh@gmail.com>
2023-07-19 23:42:34 +0300

    done footer

--- src/components/LandingFooter/LandingFooter.jsx
+++ src/components/LandingFooter/LandingFooter.jsx
@@ -1,6 +1,9 @@
 import { useSelector } from "react-redux"
 import s from './LandingFooter.module.scss'
 import Copy from 'media/img/copy.png'
+import FullLogo from 'media/img/fullLogo.png'
+import Profile from 'media/img/profile.png'
+import Telegram from 'media/img/telegram.png'
 
 export const LandingFooter = () => {
     // const {} = useSelector(state => state.)
@@ -21,6 +24,21 @@ export const LandingFooter = () => {
                     <div>
                         <a disable>0x40f6F0c0BF3c....771B036c516973145</a>
                     </div>
+                </div>
+            </div>
+            <div className={s.bottom}>
+                <div><img src={FullLogo} alt="" /></div>
+                <div>
+                    <a>
+                        <span>Follow us</span>
+                        <img src={Telegram} alt="" />
+                    </a>
+                </div>
+                <div>
+                    <button>
+                        <img src={Profile} />
+                        <span>Login</span>
+                    </button>
                 </div>
             </div>
         </footer>
