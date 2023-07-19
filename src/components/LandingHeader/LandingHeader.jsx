539d633752d083851456e1e40dc5de05d5258663
tarnovskii <artem3dgame@gmail.com>
2023-07-20 01:49:44 +0300

    complete landing

--- src/components/LandingHeader/LandingHeader.jsx
+++ src/components/LandingHeader/LandingHeader.jsx
@@ -6,10 +6,35 @@ import {routerBook} from "../../routes/routerBook";
 import {BiLogoTelegram, BiSolidWallet} from "react-icons/bi";
 import {IoDocumentText} from "react-icons/io5";
 import {HiClipboardDocumentList} from "react-icons/hi2";
+import {useScrollDirection} from "../../hooks/useScrollDirection";
+import {useEffect, useState} from "react";
 
 export const LandingHeader = () => {
+    const scrollDirection = useScrollDirection()
+    const [isHeaderTransparent, setIsHeaderTransparent] = useState(true);
+
+    useEffect(() => {
+        const handleScroll = event => {
+            window.requestAnimationFrame(() => {
+                if (isHeaderTransparent) {
+                    setIsHeaderTransparent(window.scrollY < 400);
+                } else {
+                    setIsHeaderTransparent(window.scrollY < 150);
+                }
+
+            })
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+
     return (
-        <header className={s.header}>
+        <header  data-transparent={isHeaderTransparent} data-hidden={scrollDirection === 'down'} className={s.header}>
             <Link className={s.header__logo_link} to={routerBook.main}><img src={logo} alt={'logo'}/><p>LiquiDex</p></Link>
             <nav className={s.header__links}>
                 <a href={'#'}><IoDocumentText/> Smart-contract</a>
