6700cd542d9f2392bf9330cddb9064f6979501c8
tarnovskii <artem3dgame@gmail.com>
2023-07-19 21:29:47 +0300

    complete templates

--- src/utils/initWagmi.js
+++ src/utils/initWagmi.js
@@ -1,19 +1,19 @@
-// import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-// import { configureChains, createConfig } from 'wagmi'
-// import { bsc, bscTestnet } from 'wagmi/chains'
-// import Config from 'config'
-//
-// export const initWagmi = () => {
-//   const chains = [bsc, bscTestnet]
-//   const projectId = Config().PROJECT_ID
-//
-//   const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
-//   const wagmiConfig = createConfig({
-//     autoConnect: true,
-//     connectors: w3mConnectors({ projectId, chains }),
-//     publicClient
-//   })
-//   const ethereumClient = new EthereumClient(wagmiConfig, chains)
-//
-//   return { wagmiConfig, ethereumClient, projectId }
-// }
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { configureChains, createConfig } from 'wagmi'
+import { bsc, bscTestnet } from 'wagmi/chains'
+import Config from 'config'
+
+export const initWagmi = () => {
+  const chains = [bsc, bscTestnet]
+  const projectId = Config().PROJECT_ID
+
+  const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
+  const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors: w3mConnectors({ projectId, chains }),
+    publicClient
+  })
+  const ethereumClient = new EthereumClient(wagmiConfig, chains)
+
+  return { wagmiConfig, ethereumClient, projectId }
+}
